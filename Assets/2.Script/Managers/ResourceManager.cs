4721e58229cf22160018bb1ac0cac3c21bf75709
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-07-01 04:13:41 +0900

    Apply the pool manager to the resource manager

--- Assets/2.Script/Managers/ResourceManager.cs
+++ Assets/2.Script/Managers/ResourceManager.cs
@@ -6,20 +6,42 @@ public class ResourceManager
 
     public T Load<T>(string path) where T : Object
     {
+        if (typeof(T) == typeof(GameObject))
+        {
+            int index = path.LastIndexOf('/');
+
+            if (index >= 0)
+            {
+                path = path.Substring(index + 1);
+            }
+
+            GameObject go = Managers.Pool.GetOriginal(path);
+
+            if (!ReferenceEquals(go, null))
+            {
+                return go as T;
+            }
+        }
+
         return Resources.Load<T>(path);
     }
 
     public GameObject Instantiate(string path, Transform parent = null)
     {
-        GameObject prefab = Load<GameObject>($"Prefabs/{path}");
+        GameObject original = Load<GameObject>($"Prefabs/{path}");
 
-        if (ReferenceEquals(prefab, null))
+        if (ReferenceEquals(original, null))
         {
             Debug.LogWarning($"Failed to load prefab : {path}");
             return null;
         }
 
-        GameObject go = Object.Instantiate(prefab, parent);
+        if (original.TryGetComponent(out Poolable poolObj))
+        {
+            return Managers.Pool.Pop(original, parent).gameObject;
+        }
+
+        GameObject go = Object.Instantiate(original, parent);
         int index = go.name.IndexOf("(Clone)");
         
         if (index > 0)
@@ -33,6 +55,12 @@ public class ResourceManager
     public void Destory(GameObject go)
     {
         if (ReferenceEquals(go, null)) return;
+
+        if (go.TryGetComponent(out Poolable poolObj))
+        {
+            Managers.Pool.Push(poolObj);
+            return;
+        }
 
         Object.Destroy(go);
     }
