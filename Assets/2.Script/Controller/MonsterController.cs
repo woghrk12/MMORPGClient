fe46e6f2006f751958071fa83a06b30bf68c78ca
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-07-21 22:40:34 +0900

    Add patrol functionality to the monster

--- Assets/2.Script/Controller/MonsterController.cs
+++ Assets/2.Script/Controller/MonsterController.cs
@@ -4,7 +4,103 @@ using UnityEngine;
 
 public class MonsterController : CreatureController
 {
+    #region Variables
+
+    private Coroutine coPatrol = null;
+    private Vector3Int destCellPos = Vector3Int.zero;
+
+    #endregion Variables
+
     #region Methods
+
+    #region States
+
+    protected override void UpdateIdleState()
+    {
+        if (ReferenceEquals(coPatrol, null) == false) return;
+
+        coPatrol = StartCoroutine(PatrolCo());
+    }
+
+    #endregion States
+
+    protected override void MoveToNextPos()
+    {
+        Vector3Int moveVector = destCellPos - CellPos;
+
+        if (moveVector.x != 0)
+        {
+            MoveDirection = moveVector.x > 0 ? EMoveDirection.RIGHT : EMoveDirection.LEFT;
+        }
+        else if (moveVector.y != 0)
+        {
+            MoveDirection = moveVector.y > 0 ? EMoveDirection.UP : EMoveDirection.DOWN;
+        }
+        else
+        {
+            MoveDirection = EMoveDirection.NONE;
+            State = ECreatureState.IDLE;
+            return;
+        }
+
+        Vector3Int cellPos = CellPos;
+
+        switch (MoveDirection)
+        {
+            case EMoveDirection.UP:
+                cellPos += Vector3Int.up;
+                break;
+
+            case EMoveDirection.DOWN:
+                cellPos += Vector3Int.down;
+                break;
+
+            case EMoveDirection.LEFT:
+                cellPos += Vector3Int.left;
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+                break;
+
+            case EMoveDirection.RIGHT:
+                cellPos += Vector3Int.right;
+                transform.localScale = new Vector3(1f, 1f, 1f);
+                break;
+        }
+
+        if (Managers.Map.CheckCanMove(cellPos) == true && ReferenceEquals(Managers.Obj.Find(cellPos), null) == true)
+        {
+            CellPos = cellPos;
+        }
+        else
+        {
+            State = ECreatureState.IDLE;
+        }
+    }
+
+    private IEnumerator PatrolCo()
+    {
+        yield return new WaitForSeconds(Random.Range(1, 4));
+
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3Int randPos = CellPos + new Vector3Int(Random.Range(-5, 5), Random.Range(-5, 5), 0);
+
+            if (Managers.Map.CheckCanMove(randPos) == false) continue;
+            if (ReferenceEquals(Managers.Obj.Find(randPos), null) == false) continue;
+
+            destCellPos = randPos;
+            State = ECreatureState.MOVE;
+            coPatrol = null;
+            yield break;
+        }
+
+        if (ReferenceEquals(coPatrol, null) == false)
+        {
+            StopCoroutine(coPatrol);
+            coPatrol = null;
+        }
+
+        State = ECreatureState.IDLE;
+    }
 
     #region Events
 
