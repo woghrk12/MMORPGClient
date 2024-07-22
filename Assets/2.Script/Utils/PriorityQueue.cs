a71d6ad58a2b947286e37d3c930274aa33a1648c
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-07-22 15:32:06 +0900

    Add a class to represent the heap data structure

---
+++ Assets/2.Script/Utils/PriorityQueue.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PriorityQueue<T> where T : IComparable<T>
+{
+    #region Variables
+
+    private List<T> heap = new();
+
+    #endregion Variables
+
+    #region Properties
+
+    public int Count => heap.Count;
+
+    #endregion Properties
+
+    #region Methods
+
+    public void Push(T data)
+    {
+        heap.Add(data);
+
+        int index = heap.Count - 1;
+        while (index > 0)
+        {
+            int parent = (index - 1) / 2;
+
+            if (heap[index].CompareTo(heap[parent]) < 0) break;
+
+            T temp = heap[index];
+            heap[index] = heap[parent];
+            heap[parent] = temp;
+
+            index = parent;
+        }
+    }
+
+    public T Pop()
+    {
+        T result = heap[0];
+
+        int lastIndex = heap.Count - 1;
+        heap[0] = heap[lastIndex];
+        heap.RemoveAt(lastIndex);
+        lastIndex--;
+
+        int index = 0;
+        while (true)
+        {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+
+            int child = index;
+            if (left <= lastIndex && heap[child].CompareTo(heap[left]) < 0)
+            {
+                child = left;
+            }
+            if (right <= lastIndex && heap[child].CompareTo(heap[right]) < 0)
+            {
+                child = right;
+            }
+
+            if (child == index) break;
+
+            T temp = heap[index];
+            heap[index] = heap[child];
+            heap[child] = temp;
+
+            index = child;
+        }
+
+        return result;
+    }
+
+    #endregion Methods
+}
