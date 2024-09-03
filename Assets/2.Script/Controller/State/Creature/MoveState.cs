3303aa0b65850388add25b48d1d92b0f8015fbaf
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-09-03 19:57:52 +0900

    Fix the issue where the character jitter when moving against a wall

--- Assets/2.Script/Controller/State/Creature/MoveState.cs
+++ Assets/2.Script/Controller/State/Creature/MoveState.cs
@@ -35,12 +35,14 @@ namespace Creature
             animator.SetBool(AnimatorKey.Creature.IS_MOVE_HASH, true);
         }
 
-        public override void OnFixedUpdate()
+        public override void OnUpdate()
         {
             Vector3 destPos = new Vector3(controller.CellPos.x, controller.CellPos.y, 0f) + new Vector3(0.5f, 0.5f, 0f);
             Vector3 moveDir = destPos - transform.position;
 
-            transform.position += controller.MoveSpeed * Time.fixedDeltaTime * moveDir.normalized;
+            if (moveDir.sqrMagnitude < (controller.MoveSpeed * Time.deltaTime) * (controller.MoveSpeed * Time.deltaTime)) return;
+
+            transform.position += controller.MoveSpeed * Time.deltaTime * moveDir.normalized;
         }
 
         public override void OnExit()
