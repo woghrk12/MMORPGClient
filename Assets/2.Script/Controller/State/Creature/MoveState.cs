de8c5b65a19a582942556178013d6518d8107f13
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2024-09-04 20:55:51 +0900

    Fix the issue where the character's position become misaligned

--- Assets/2.Script/Controller/State/Creature/MoveState.cs
+++ Assets/2.Script/Controller/State/Creature/MoveState.cs
@@ -40,9 +40,14 @@ namespace Creature
             Vector3 destPos = new Vector3(controller.CellPos.x, controller.CellPos.y, 0f) + new Vector3(0.5f, 0.5f, 0f);
             Vector3 moveDir = destPos - transform.position;
 
-            if (moveDir.sqrMagnitude < (controller.MoveSpeed * Time.deltaTime) * (controller.MoveSpeed * Time.deltaTime)) return;
-
-            transform.position += controller.MoveSpeed * Time.deltaTime * moveDir.normalized;
+            if (moveDir.sqrMagnitude < (controller.MoveSpeed * Time.deltaTime) * (controller.MoveSpeed * Time.deltaTime))
+            {
+                transform.position = destPos;
+            }
+            else
+            {
+                transform.position += controller.MoveSpeed * Time.deltaTime * moveDir.normalized;
+            }
         }
 
         public override void OnExit()
