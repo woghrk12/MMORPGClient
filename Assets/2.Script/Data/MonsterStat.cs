6087493f81a51882ddb0d1d8a2f65a63815923c3
LAPTOP-7K9L41UJ\hjh22 <hjh222427@gmail.com>
2025-03-10 20:16:41 +0900

    Fix the monster stats to a single value regardless of level

--- Assets/2.Script/Data/MonsterStat.cs
+++ Assets/2.Script/Data/MonsterStat.cs
@@ -1,13 +1,11 @@
-using System.Collections.Generic;
-
 namespace Data
 {
     public class MonsterStat
     {
         public int ID;
         public string Name;
-        public Dictionary<int, int> MaxHpDictionary = new();
-        public Dictionary<int, int> AttackPowerDictionary = new();
+        public int MaxHP;
+        public int AttackPower;
         public int PatrolRange;
         public int DetectionRange;
         public int ChaseRange;
